35f6af613c34039f4754d824a22ec1ba0f39ec08
Alexey Awdonin <aaudonin@exadel.com>
2013-12-09 18:17:05 +0300

    pr15 created but NOT solved

--- solutions/pr15.py
+++ solutions/pr15.py
@@ -1,2 +1,14 @@
+def find_num_routes(boxSize):
+
+    num_routes = 0
+
+    for i in range(boxSize):
+        for j in range(boxSize):
+            for k in range(boxSize):
+                for n in range(boxSize):
+                    pass
+
+    return num_routes
+
 def solve():
     pass
