e7dc8ce16f79736f31eb8b626d4468f667e6b788
Alexey Awdonin <aaudonin@exadel.com>
2013-12-11 17:51:03 +0300

    pr25 solved

--- solutions/pr25.py
+++ solutions/pr25.py
@@ -3,11 +3,16 @@ def fibonacci(num):
         return 1
     return fibonacci(num-1) + fibonacci(num-2)
 
+def fibonacci_cheat(n):
+    phi = (1 + pow(5, 0.5))/2
+    return int((pow(phi, n) - pow(-phi, -n))/pow(5, 0.5))
+
 def solve():
     i = 1
-    last_len = len(str(fibonacci(i)))
-    while last_len != 1000:
-        i += 1
-        last_len = len(str(fibonacci(i)))
 
-    print i
+    a, b = 1, 1
+    x = 2
+    while len(str(b)) < 1000:
+        a, b = b, a+b
+        x += 1
+    print x
